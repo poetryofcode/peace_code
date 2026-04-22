98aec328bb65b10bb6ec93268e25faa1903400c0
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2026-04-22 12:16:32 -0400

    2. test even

--- peacecode.js
+++ peacecode.js
@@ -2,20 +2,30 @@
 The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 
 What if the string is empty? Then the result should be empty object literal, {}.
-*/
 
 let string = "abc";
 
 function count(string) {
-  let obj = {};
-  if (string.length === 0) return obj;
-  for (let i = 0; i < string.length; i++) {
-    let char = string[i];
-    if (obj[char]) {
-      obj[char]++;
-    } else obj[char] = 1;
+    let obj = {};
+    if (string.length === 0) return obj;
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i];
+        if (obj[char]) {
+            obj[char]++;
+        } else obj[char] = 1;
+    }
+    return obj;
+}
+*/
+
+let n = -3;
+
+function testEven(n) {
+  if (n % 1 === 0) {
+    if (n % 2 === 0) return true;
+    if (n % 2 === 1) return false;
   }
-  return obj;
+  return false;
 }
 
-console.log(count(string));
+console.log(testEven(n));
